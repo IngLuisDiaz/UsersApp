2ec35ebf54931b526c7e05a9abf242ed3eb71bec
IngLuisDiaz <luiscarlosdiazflorez@gmail.com>
2024-12-06 17:54:50 -0500

    CreacionArchivo reducerstate sin editar ni enlazar

---
+++ src/reducers/usersReducers.js
@@ -0,0 +1,10 @@
+export const usersReducer =( state=[], action)=>{
+  switch(action.type){
+    case 'ADD_USER':
+      return [...state, action.payload]
+    case 'DELETE_USER':
+      return state.filter(user=>user.id!==action.payload)
+    default:
+      return state
+  }
+}
